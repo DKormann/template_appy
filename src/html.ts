f742605ceb0e6a6b84184041ba02a207aa9305ab
DKormann <dominikormann@outlook.com>
2025-09-12 22:24:54 +0200

    update

--- src/html.ts
+++ src/html.ts
@@ -1,12 +1,12 @@
 import { Writable } from "./store"
 
-export type htmlKey = 'innerText'|'onclick'|'children'|'class'|'id'|'contentEditable'|'eventListeners'|'color'|'background' | 'style'
+export type htmlKey = 'innerText'|'onclick' | 'oninput' | 'onkeydown' |'children'|'class'|'id'|'contentEditable'|'eventListeners'|'color'|'background' | 'style' | 'placeholder'
 
 export const htmlElement = (tag:string, text:string, cls:string = "", args?:Partial<Record<htmlKey, any>>):HTMLElement =>{
 
   const _element = document.createElement(tag)
   _element.innerText = text
-  if (cls) _element.classList.add(...cls.split('.').filter(x=>x))
+  // if (cls) _element.classList.add(...cls.split('.').filter(x=>x))
   if (args) Object.entries(args).forEach(([key, value])=>{
     if (key === 'parent'){
       (value as HTMLElement).appendChild(_element)
@@ -23,80 +23,148 @@ export const htmlElement = (tag:string, text:string, cls:string = "", args?:Part
       Object.entries(value as Record<string, string>).forEach(([key, value])=>{
         _element.style.setProperty(key, value)
       })
+    }else if (key === 'class'){
+      _element.classList.add(...(value as string).split('.').filter(x=>x))
     }else{
-      _element[(key as 'innerText' | 'onclick' | 'id' | 'contentEditable')] = value
+      _element[(key as 'innerText' | 'onclick' | 'oninput' | 'id' | 'contentEditable')] = value
     }
   })
   return _element
 }
 
 
+type HTMLArg = string | number | HTMLElement | Partial<Record<htmlKey, any>> | Writable<any> | Promise<HTMLArg> | HTMLArg[]
 
 
-
-export const html = (tag:string, ...cs:(string | HTMLElement | Partial<Record<htmlKey, any>>|Writable<any>)[]):HTMLElement=>{
-  let content = ''
+export const html = (tag:string, ...cs:HTMLArg[]):HTMLElement=>{
   let children: HTMLElement[] = []
   let args: Partial<Record<htmlKey, any>> = {}
-  for (let c of cs){
-    if (typeof c === 'string') content += c
-    else if (typeof c === 'number') content += c
-    else if (c instanceof Writable){
+
+  const add_arg = (arg:HTMLArg)=>{
+    if (typeof arg === 'string') children.push(htmlElement("span", arg))
+    else if (typeof arg === 'number') children.push(htmlElement("span", arg.toString()))
+    else if (arg instanceof Writable){
+      const el = span({class:"writable-container"})
+      arg.subscribe((value)=>{
+        el.innerHTML = ""
+        el.appendChild(span(value, {class:"writable-value"}))
+      })
+      children.push(el)
+    }
+    else if (arg instanceof Promise){
       const el = span()
-      c.subscribe((value)=>{
-        console.log("value", value)
+      arg.then((value)=>{
         el.innerHTML = ""
         el.appendChild(span(value))
       })
       children.push(el)
     }
-    else if (c instanceof HTMLElement) children.push(c)
-    else args = {...args, ...c}
+    else if (arg instanceof HTMLElement) children.push(arg)
+    else if (arg instanceof Array) arg.forEach(add_arg)
+    else args = {...args, ...arg}
   }
-  return htmlElement(tag, content, "", {...args, children})
+  for (let arg of cs){
+    add_arg(arg)
+  }
+  return htmlElement(tag, "", "", {...args, children})
 }
 
 
-export type HTMLGenerator = (...cs:(string | HTMLElement | Partial<Record<htmlKey, any>> | Writable<any>)[]) => HTMLElement
+export type HTMLGenerator<T extends HTMLElement = HTMLElement> = (...cs:HTMLArg[]) => T
 
-export const p:HTMLGenerator = (...cs)=>html("p", ...cs)
-export const h1:HTMLGenerator = (...cs)=>html("h1", ...cs)
-export const h2:HTMLGenerator = (...cs)=>html("h2", ...cs)
-export const h3:HTMLGenerator = (...cs)=>html("h3", ...cs)
-export const h4:HTMLGenerator = (...cs)=>html("h4", ...cs)
-
-export const div:HTMLGenerator = (...cs)=>html("div", ...cs)
-export const button:HTMLGenerator = (...cs)=>html("button", ...cs) as HTMLButtonElement
-export const span:HTMLGenerator = (...cs)=>html("span", ...cs) as HTMLButtonElement
-export const input:HTMLGenerator = (...cs)=>html("input", ...cs) as HTMLInputElement
-export const textarea:HTMLGenerator = (...cs)=>html("textarea", ...cs) as HTMLTextAreaElement
-
-export const table:HTMLGenerator = (...cs)=>html("table", ...cs)
-export const tr:HTMLGenerator = (...cs)=>html("tr", ...cs)
-export const td:HTMLGenerator = (...cs)=>html("td", ...cs)
-export const th:HTMLGenerator = (...cs)=>html("th", ...cs)
+const newHtmlGenerator = <T extends HTMLElement>(tag:string)=>(...cs:HTMLArg[]):T=>html(tag, ...cs) as T
 
 
-export const popup = (dialogfield: HTMLElement)=>{
 
-  const popupbackground = htmlElement("div", "", "popup-background");
+export const p:HTMLGenerator<HTMLParagraphElement> = newHtmlGenerator("p")
+export const h1:HTMLGenerator<HTMLHeadingElement> = newHtmlGenerator("h1")
+export const h2:HTMLGenerator<HTMLHeadingElement> = newHtmlGenerator("h2")
+export const h3:HTMLGenerator<HTMLHeadingElement> = newHtmlGenerator("h3")
+export const h4:HTMLGenerator<HTMLHeadingElement> = newHtmlGenerator("h4")
+
+export const div:HTMLGenerator<HTMLDivElement> = newHtmlGenerator("div")
+export const button:HTMLGenerator<HTMLButtonElement> = newHtmlGenerator("button")
+export const span:HTMLGenerator<HTMLSpanElement> = newHtmlGenerator("span")
+export const textarea:HTMLGenerator<HTMLTextAreaElement> = newHtmlGenerator("textarea")
+
+export const table:HTMLGenerator<HTMLTableElement> = newHtmlGenerator("table")
+export const tr:HTMLGenerator<HTMLTableRowElement> = newHtmlGenerator("tr")
+export const td:HTMLGenerator<HTMLTableCellElement> = newHtmlGenerator("td")
+export const th:HTMLGenerator<HTMLTableCellElement> = newHtmlGenerator("th")
+
+
+export const input:HTMLGenerator<HTMLInputElement> = (...cs)=>{
+
+  const writable = cs.find(c=>c instanceof Writable) as Writable<string>
+
+  const content = cs.filter(c=>typeof c == 'string').join(' ')
+
+  const el = html("input", ...cs) as HTMLInputElement
+
+  if (writable){
+    writable.subscribe(v=>{
+      if (el.value!=v.toString()){
+        el.value = v.toString()
+      }
+    });
+
+    el.onkeydown = (e)=>{
+      if (e.key == "Enter"){
+        writable.set(el.value)
+      }
+    }
+
+  }else{
+    el.value = content
+  }
+  return el
+}
+
+
+
+export const popup = (...cs:HTMLArg[])=>{
+
+  const dialogfield = div(...cs)
+
+  // const popupbackground = htmlElement("div", "", "popup-background");
+  const popupbackground = div(
+    {style:{
+      "position": "fixed",
+      "top": "0",
+      "left": "0",
+      "width": "100%",
+      "height": "100%",
+      "background": "rgba(166, 166, 166, 0.5)",
+      "display": "flex",
+      "justify-content": "center",
+      "align-items": "center",
+
+    }}
+  )
 
   popupbackground.appendChild(dialogfield);
   document.body.appendChild(popupbackground);
   popupbackground.onclick = () => {
     popupbackground.remove();
   }
-  dialogfield.classList.add("popup-dialog");
-  popupbackground.appendChild(htmlElement("div", "close", "popup-close", {
-    onclick: () => {
-      popupbackground.remove();
-    }
-  }))
+
+  dialogfield.style.background = "var(--bg)"
+  dialogfield.style.color = "var(--color)"
+  dialogfield.style.padding = "1em"
+  dialogfield.style.paddingBottom = "2em"
+  dialogfield.style.borderRadius = "1em"
+
+
+  // popupbackground.appendChild(p("close", {
+  //   onclick: () => {
+  //     popupbackground.remove();
+  //   }
+  // }))
 
   dialogfield.onclick = (e) => {
     e.stopPropagation();
   }
 
-  return ()=>popupbackground.remove()
+  return popupbackground
 
 }
--- src/store.ts
+++ src/store.ts
@@ -1,39 +1,81 @@
 export class Writable <T> {
-  private value: T
-  private listeners: Array<(value: T) => void> = []
+  private value: Promise<T>
+  public resolved: T
+  private previous?: T
+  private listeners: Array<(value: T) => void> = [] 
+  private listeners_once: Array<(value: T) => void> = []
 
-  constructor(initialValue: T) {
-    this.value = initialValue
+  constructor(initialValue: T | Promise<T>) {
+
+    this.set(initialValue)
   }
 
-  get(): T {
-    return this.value
-  }
+  async set (newValue: T | Promise<T>, force = false){
 
-  set(newValue: T, force = false): void {
-    if (!force && newValue === this.value) return
+    if (newValue instanceof Promise){
+      if (!force && newValue == this.value) return
+      this.previous = this.resolved
+      this.resolved = undefined
 
-    for (const listener of this.listeners) {
-      listener(newValue)
+      this.value = newValue
+      return this.value.then(v=>{
+        if (newValue != this.value) return
+        this.resolved = v
+        if (v == this.previous)return
+        this.listeners.forEach(l=>l(v))
+        this.listeners_once.forEach(l=>l(v))
+        this.listeners_once = []
+      })
+    }else{
+      if (!force && newValue == this.resolved) return
+      this.value = Promise.resolve(newValue)
+      this.resolved = newValue
+      this.listeners.forEach(l=>l(newValue))
+      this.listeners_once.forEach(l=>l(newValue))
+      this.listeners_once = []
     }
-    this.value = newValue
   }
 
-
-  update(updater: (value: T) => T, force = false): void {
-    const newValue = updater(this.value)
-    this.set(newValue, force)
-  }
-
-  subscribe(listener: (value: T) => void) {
+  subscribe(listener: (value: T) => any){
     this.listeners.push(listener)
-    listener(this.value)
+    if (this.resolved != undefined) listener(this.resolved)
   }
 
-  subscribeLater(listener: (value: T) => void){
+  subscribeLater(listener: (value: T) => any){
     this.listeners.push(listener)
+  }
+
+  get(): Promise<T>{
+    return new Promise((resolve, reject)=>{
+
+      if(this.resolved != undefined) return resolve(this.resolved)
+      
+      let sub = (res:T) => {
+        resolve(res)
+      }
+      
+      this.listeners_once.push(sub)
+    })
+  }
+
+  then<U>(fn: (value: T) => U): Promise<U>{
+    return this.get().then(fn)
+  }
+
+  async update(fn: (value:T) => T | Promise<T>, force = false){
+    return this.set(await this.get().then(fn), force)
+  }
+
+
+  map<U>(mapper: (value: T) => U){
+    let res = new Writable<U>(new Promise(()=>{}))
+    this.subscribe(v=>{
+      res.set(mapper(v))
+    })
+    return res
   }
 }
+
 
 export class Stored<T> extends Writable<T> {
   key: string
@@ -43,17 +85,12 @@ export class Stored<T> extends Writable<T> {
     }
     super(initialValue)
     this.key = key
-  }
 
-  set (newValue: T): void {
-    if (JSON.stringify(this.get()) !== JSON.stringify(newValue)) {
-      super.set(newValue)
-      localStorage.setItem(this.key, JSON.stringify(newValue))
-    }
+    this.subscribe(v=>{
+      localStorage.setItem(this.key, JSON.stringify(v))
+    })
   }
 }
-
-
 
 export interface Readable<T> {
   get(): T
@@ -61,4 +98,78 @@ export interface Readable<T> {
   subscribeLater(listener: (value: T) => void): void
 }
 
+export type Consumer = {
+  resolve : (value:any) => void
+  reject? : (e:Error) => void
+}
 
+export class CachedStore {
+
+  requests: Map<bigint, Consumer[]>
+  subscriptions: Map<bigint, Writable<any>>
+
+
+  constructor(){
+    this.requests = new Map<bigint, Consumer[]>()
+    this.subscriptions = new Map<bigint, Writable<any>>()
+
+  }
+
+  request (key:bigint) : Promise<any> {
+    return new Promise((resolve, reject)=>{
+
+      if (this.subscriptions.has(key)) resolve(this.subscriptions.get(key).get())
+      if (!this.requests.has(key)) this.requests.set(key, [])
+      this.requests.get(key).push({resolve,reject})
+    })
+  }
+
+  subscribe(key:bigint){
+    
+    if (!this.subscriptions.has(key))
+      this.subscriptions.set(key, new Writable<any>(null))
+    return this.subscriptions.get(key)
+  }
+
+  reject(key:bigint, e:Error){
+    this.requests.get(key)?.forEach(r=>{if (r.reject) r.reject(e)})
+    this.requests.delete(key)
+    this.subscriptions.get(key).set(e)
+  }
+
+  produce(key:bigint, value:any){
+    this.requests.get(key)?.forEach(r=>r.resolve(value))
+    this.requests.delete(key)
+    if (!this.subscriptions.has(key))
+      this.subscriptions.set(key, new Writable<any>(null))
+    this.subscriptions.get(key)!.set(value)
+  }
+  
+
+}
+
+
+// export function sIf <T> (cond:Writable<boolean>, then:Readable<T>, otherwise?:Readable<T>) : Readable<T | null> {
+
+//   let res = new Writable<T | null>(null)
+//   cond.subscribe(c=>{
+//     if (c) res.set(then.get())
+//     else if (otherwise) res.set(otherwise.get())
+//     else res.set(null)
+//   })
+//   then.subscribe(v=>{
+//     if (cond.get()) res.set(v)
+//     else if (otherwise) res.set(otherwise.get())
+//     else res.set(null)
+//   })
+//   return res
+// }
+
+// export function sMap <T,U> (source:Readable<T>, mapper:(value:T)=>U) : Writable<U> {
+
+//   let res = new Writable<U>(mapper(source.get()))
+//   source.subscribe(v=>{
+//     res.set(mapper(v))
+//   })
+//   return res
+// }
